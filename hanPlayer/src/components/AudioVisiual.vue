2b583974dd884b80c8ccb18281b1cbf9ac72e3e3
TheLastMorty <1483625333@qq.com>
2023-07-21 17:31:23 +0800

    audio play

--- hanPlayer/src/components/AudioVisiual.vue
+++ hanPlayer/src/components/AudioVisiual.vue
@@ -1,43 +1,79 @@
 <template>
     <div>
-        <audio controls id="audio" ></audio>
+        <ul>
+            <li v-for="(item,index) of data" :key="index" @click="changeMusic(item.uri)">{{item.name}}</li>
+        </ul>
     </div>
 </template>
 
 <script setup lang="ts">
-    import {ref,onMounted } from "vue";
 
-    let src=ref("http://47.113.204.82/music/mecury.mp3")
+    let context = new AudioContext;
+    let audio:Audio;
+    let source;
+    let analyser;
+    let fftsize:number=256;
+    let dataArray=new Array(fftsize/2)
+    let timeInterval;
 
-    onMounted(()=>{
-        let audio = document.getElementById('audio');
-        // audio.crossOrigin = 'anonymous';
-        audio.src=src.value;
-        let audioObject = new Audio(src.value)
-        const AudioContext = window.AudioContext || window.webkitAudioContext;
-        const ctx = new AudioContext();
-        const analyser = ctx.createAnalyser();
-        analyser.fftSize = 256;
-        const source = ctx.createMediaElementSource(audio);
-        // source.connect(analyser);
-        // var bufferLength = analyser.frequencyBinCount;
-        // var dataArray = new Uint8Array(bufferLength);
-        // analyser.connect(ctx.destination);
-        // analyser.getByteFrequencyData(dataArray)
-        // console.log('data :>> ', dataArray);
 
-        audio?.addEventListener("play",(event)=>{
-            console.log("play");
-            
-            
+    import {reactive } from "vue";
+    interface Data{
+        name:string;
+        uri:string;
+    }
+    let musicList:Data[]=new Array();
+    musicList.push({name:"水星记",uri:"http://47.113.204.82/music/mecury.mp3"})
+    musicList.push({name:"凄美地",uri:"http://47.113.204.82/music/qimeidi.mp3"})
+
+    let data=reactive(musicList)
+
+    function changeMusic(musicSrc:string){
+        // var context = new AudioContext;
+            //加载媒体
+        // console.log(musicSrc)
+        var audio = new Audio(musicSrc);
+        audio.addEventListener("play",(event)=>{
+            console.log(musicSrc)
         })
-        audio?.addEventListener("ended",(event)=>{
-            console.log("end");
-        })
-        audio?.addEventListener("pause",(evnet)=>{
-            console.log("pause");
-        })
-    })
-    
+        audio.play()
+    }
+
+    function createTimeInterval(){
+        return setInterval(()=>{
+            analyser.getByteFrequencyData(dataArray);
+            console.log(dataArray)
+        },1000)
+    }
+
+    function clearTimeInterval(){
+        if(timeInterval){
+            clearInterval(timeInterval)
+        }
+    }
+
+    function play(){
+            var context = new AudioContext;
+            //加载媒体
+            var audio = new Audio("http://47.113.204.82/music/mecury.mp3");
+            // var audio=document.getElementById
+            //创建节点
+            var source = context.createMediaElementSource(audio);
+            var analyser = context.createAnalyser();
+            //连接：source → analyser → destination
+            source.connect(analyser);
+            analyser.connect(context.destination);
+            analyser.fftSize = 256;
+            var length = analyser.fftSize;
+            //创建数据
+            // var dataArray = new Uint8Array(length);
+            audio.play()
+            analyser.getByteFrequencyData(dataArray);
+            console.log(dataArray)
+            var a=setInterval(() => {
+                analyser.getByteFrequencyData(dataArray);
+                console.log(dataArray)
+            }, 1000);
+        }
 
 </script>
